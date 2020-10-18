6b03fd4f9b5be8e72418e9fb8e3453ddbc325c0a
jonferraz <71293969+jonferraz@users.noreply.github.com>
2020-10-18 18:16:53 -0300

    Initial commit

---
+++ AP2_Q01_JS/Função.js
@@ -0,0 +1,24 @@
+let num = document.querySelector("input#numero")
+let lista = document.querySelector("select#Resultado")
+let res = document.querySelector('div#res')
+let valores = []
+
+function media (n1, n2, n3){​​​​​
+    return n1 + n2 + n3 
+}​​​​​
+    console.log(media(any, any, any))
+
+function isnumero(n) {
+       if(Number(any) = 1 && Number(any) <= 100){
+           return true
+       } else{
+           return false
+       }
+}
+function adicionar() {
+    if(isnumero(nun.value)) {
+
+    } else{
+        window.alert(" valor invalido ou ja encontrado na lista")
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AP2_Pratica_de_Programação
+ AP2
