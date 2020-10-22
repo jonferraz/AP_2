14a175430dcc65d067a906108b5ba4519ebebc81
jonferraz <71293969+jonferraz@users.noreply.github.com>
2020-10-22 01:25:00 -0300

    Update Script3.js

--- Script3.js
+++ Script3.js
@@ -1,29 +1,15 @@
-function calcula(value)
-{
-	  por = 0;
-    value = parseFloat(value);    
-		if (value <=280)
-    {
-    	por = 20;
-    }
-    else if (value > 280 && value <= 700)
-    {
-    	por = 15;
-    }
-    else if (value > 700 && value <= 1500)
-    {
-    	por = 10;
-    }
-    else if (value > 1500)
-    {
-    	por = 5;
-    }
-    resp = value + ((value * por) / 100);
-    document.getElementById("valor").innerHTML = "Salário: " + value;
-    document.getElementById("calculo").innerHTML = "Cálculo de aumento: " + ((value * por) / 100);
-    document.getElementById("porcetagem").innerHTML = "Porcentagem: " + por + "%";    
+  var n1 = 9;
+  var n2 = 6;
+  var n3 = 6;
+
+  var media = (n1 + n2 + n3) /3;
+
+    resp = media + ((n1 + n2 + n3) /3);
+    document.getElementById("Nome do aluno").innerHTML = "Jonathan";
+    document.getElementById("Nota").innerHTML = "Cálculo de aumento: " + media;
+    document.getElementById("Aprovado").innerHTML = "Porcentagem: " + por + "%";    
     document.getElementById("reposta").innerHTML = "Valor Final: " + resp;
     
-    return resp;
-}
+    return media;
+
 
