939a130e2249f7bfa4d4543168d4ac0736f227c9
jonferraz <71293969+jonferraz@users.noreply.github.com>
2020-10-21 23:07:40 -0300

    Create Script2.js

---
+++ Script2.js
@@ -0,0 +1,29 @@
+function calcula(value)
+{
+	  por = 0;
+    value = parseFloat(value);    
+		if (value <=280)
+    {
+    	por = 20;
+    }
+    else if (value > 280 && value <= 700)
+    {
+    	por = 15;
+    }
+    else if (value > 700 && value <= 1500)
+    {
+    	por = 10;
+    }
+    else if (value > 1500)
+    {
+    	por = 5;
+    }
+    resp = value + ((value * por) / 100);
+    document.getElementById("valor").innerHTML = "Salário: " + value;
+    document.getElementById("calculo").innerHTML = "Cálculo de aumento: " + ((value * por) / 100);
+    document.getElementById("porcetagem").innerHTML = "Porcentagem: " + por + "%";    
+    document.getElementById("reposta").innerHTML = "Valor Final: " + resp;
+    
+    return resp;
+}
+
