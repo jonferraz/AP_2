68bea41864a6c9b5d445a5c524c1d4b74b67f072
jonferraz <71293969+jonferraz@users.noreply.github.com>
2020-10-24 15:05:09 -0300

    Create Script3.js

---
+++ Script3.js
@@ -0,0 +1,7 @@
+var a = Number(window.prompt('Digite um valor para A: '))
+var b = Number(window.prompt( 'Digite um valor para B: '))
+var res = a == b ? a + b:a*b
+
+document.write(`A variável C é `,res) 
+
+
