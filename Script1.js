eea4d4cb9fe4ddc93dfba2673c44fdfe71e43499
jonferraz <71293969+jonferraz@users.noreply.github.com>
2020-10-24 13:14:29 -0300

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -1,6 +1,6 @@
-var n1 = 9;
-var n2 = 6;
-var n3 = 5;
+var n1 = Number(window.prompt('Digite 1°'));//(parseFoat para converster numero)salvar nota do aluno
+var n2 = Number(window.prompt('digite 2°'));
+var n3 = Number(window.prompt('Digite 3°'));
 
 var media = (n1 + n2 + n3) /3;
 
