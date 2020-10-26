7438992ab5db3dd8b7207eeb2e44597a132ea257
jonferraz <71293969+jonferraz@users.noreply.github.com>
2020-10-26 00:20:19 -0300

    Update Script8.js

--- Script8.js
+++ Script8.js
@@ -1,7 +1,7 @@
 
 var x = Number(window.prompt('Valor real'));
 var soma = 0;
-for (var i = 0; i < 19 ; ) {
+for (var i = 0; i < 20 ; ) {
    numero=prompt("Entre com o numero: ");
    numero = parseInt(numero);
    while (numero > 0) { 
