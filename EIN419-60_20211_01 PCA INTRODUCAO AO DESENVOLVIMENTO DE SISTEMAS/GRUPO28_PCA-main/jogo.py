1e9b1da2787cab361e3cda08ea0543bd515fbe20
jonferraz <71293969+jonferraz@users.noreply.github.com>
2021-05-15 19:41:54 -0300

    Update jogo.py

--- EIN419-60_20211_01 PCA INTRODUCAO AO DESENVOLVIMENTO DE SISTEMAS/GRUPO28_PCA-main/jogo.py
+++ EIN419-60_20211_01 PCA INTRODUCAO AO DESENVOLVIMENTO DE SISTEMAS/GRUPO28_PCA-main/jogo.py
@@ -74,3 +74,9 @@ f_jogo()
 print('\033[;1m' + '\033[1;33m' + 'Fim do jogo!! Seu resultado foi:' + '\033[0;0m')
 print(f'\nA quantidade de respostas certas foram: {len(listaCertas)}\nA quantidade de respostas erradas foram: {len(listaErradas)}')
 linha()
+
+
+
+
+
+
